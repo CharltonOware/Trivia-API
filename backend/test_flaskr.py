8b839d6721f4ff1c73795b73910e6421e8e7b175
Charlton Omondi <omondioware@gmail.com>
2022-06-15 13:19:53 +0300

    include environment variables and implement test for get_paginated_questions

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,10 +2,19 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
+from dotenv import load_dotenv
 
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+#Grab folder where script runs
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir,'.env'))
+
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +24,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format(DB_USER,DB_PASSWORD,DB_HOST, self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +42,24 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['page'],1)
+        self.assertTrue(data['question'])
+        self.assertTrue(data['answer'])
+        self.assertTrue(data['difficulty'])
+
+    def test_404_requested_page_outside_range(self):
+        res = self.client().get('/questions?page=100')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
 
 
 # Make the tests conveniently executable
